1893ff444070d1d7a09fff62e49bda9a040be18e
TheeCapain <AugustHauerslev@outlook.com>
2021-05-11 14:55:25 +0200

    Update to manageMember

--- src/ManageMembers.java
+++ src/ManageMembers.java
@@ -2,16 +2,24 @@ import java.util.ArrayList;
 
 public class ManageMembers {
     //Skal ændres til at modtage en member når den er lavet
-    private ArrayList<String> members = new ArrayList<>();
+    private ArrayList<Member> members = new ArrayList<>();
+    Member member = new Member();
 
     //Creates new member and adds to the ArrayList
-    public void createNewMember(){
-
-
+    public void createNewMember() {
+        name = member.setName();
+        age = member.setName();
+        ID = member.setID();
+        membershipType = member.set
+        members.add(member(name, age, ID, membershipType, memberStatus, memberCash));
     }
 
     //Deletes a member from the members List
-    public void deleteMember(){
+    public void deleteMember() {
+
+    }
+
+    public void printMembersList(){
 
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class UI {
+    Scanner scan =  new Scanner(System.in);
+
+    public void display(String message){
+        System.out.println(message);
+    }
+    public void display(int number){
+        System.out.println(number);
+    }
+
+    public void scanInt(){
+
+    }
+
 }
