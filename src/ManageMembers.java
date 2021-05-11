3edf7968759662f8d90a9c267c3db006ab01ad4d
TheeCapain <AugustHauerslev@outlook.com>
2021-05-11 14:12:26 +0200

    test

--- src/ManageMembers.java
+++ src/ManageMembers.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 public class ManageMembers {
-    //Skal ændres til at modtage en member
-    ArrayList<String> members = new ArrayList<>();
-    // @Author August
+    //Skal ændres til at modtage en member når den er lavet
+    private ArrayList<String> members = new ArrayList<>();
 
     //Creates new member and adds to the ArrayList
     public void createNewMember(){
+
 
     }
 
