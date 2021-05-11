d79c2d01188ef76ca1b565b7bccbc579c007f347
TheeCapain <AugustHauerslev@outlook.com>
2021-05-11 14:06:02 +0200

    Added ManageMembers

---
+++ src/ManageMembers.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ManageMembers {
+    //Skal ændres til at modtage en member
+    ArrayList<String> members = new ArrayList<>();
+    // @Author August
+
+    //Creates new member and adds to the ArrayList
+    public void createNewMember(){
+
+    }
+
+    //Deletes a member from the members List
+    public void deleteMember(){
+
+    }
+}
