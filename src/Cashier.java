169d4e8f22459f863be9a08a8dfa66902b39a3dd
Jens-k-m-m <jensmonteiro@gmail.com>
2021-05-19 10:23:39 +0200

    Ja

--- src/Cashier.java
+++ src/Cashier.java
@@ -1,17 +1,7 @@
 import java.util.ArrayList;
 
 public class Cashier {
-  //For aktive medlemmer er kontingentet for ungdomssvømmere (under 18 år) 1000 kr. årligt,
-  //For seniorsvømmere (18 år og over) 1600 kr. årligt.
-  //For medlemmer over 60 år gives der 25 % rabat af seniortaksten.
-  //For passivt medlemskab er taksten 500 kr. årligt.
-  public void setMembershipPayment(Ui ui, ArrayList<Member> members, Member member) {
-    boolean keepRunning = true;
-    for (int i = 0; i < members.size(); i++) {
-      if (members.get(i).getMemberStatus().equals("Active")) ;
 
-    }
-  }
 
   public void sortActiveMember(Ui ui, ArrayList<Member> members, ArrayList<Member> activeMember, Member member, FileHandler fileHandler) {
     fileHandler.readFile(ui, members, member);
@@ -28,5 +18,13 @@ public class Cashier {
       System.out.println(activeMember.get(i).toString());
     }
   }
-}
 
+  //For aktive medlemmer er kontingentet for ungdomssvømmere (under 18 år) 1000 kr. årligt,
+  //For seniorsvømmere (18 år og over) 1600 kr. årligt.
+  //For medlemmer over 60 år gives der 25 % rabat af seniortaksten.
+  //For passivt medlemskab er taksten 500 kr. årligt.
+  public void setMembershipPayment(ArrayList<Member> activeMembers) {
+
+
+  }
+}
--- src/Controller.java
+++ src/Controller.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Controller {
     Ui ui = new Ui();
     ArrayList<Member> members = new ArrayList<>();
-    ArrayList<Member> activeMember = new ArrayList<>();
+    ArrayList<Member> activeMembers = new ArrayList<>();
     FileHandler fileHandler = new FileHandler();
     Member member = new Member();
     ForemanController foreman = new ForemanController();
@@ -27,8 +27,10 @@ public class Controller {
             keepRunning = true;
 
             switch (choice) {
-                case "1" -> foreman.initializeForeman(ui, members, menu,fileHandler);
-                case "2" -> cashier.sortActiveMember(ui,members,activeMember,member,fileHandler);
+                case "1" -> foreman.initializeForeman(ui, members, menu, fileHandler);
+                case "2" -> {cashier.sortActiveMember(ui, members, activeMembers, member, fileHandler);
+                cashier.setMembershipPayment(activeMembers);
+            }
                 case "3" -> coach.coachController();
                 case "9" -> {
                     ui.display("Bye");
