7bd86a704055d2e1168803d68807acaaa36fd185
Jens-k-m-m <jensmonteiro@gmail.com>
2021-05-24 10:37:32 +0200

    Ja

--- src/Bye.java
+++ src/Bye.java
@@ -1,26 +1,27 @@
 public class Bye {
-
+//Jens
   public void bye(Ui ui) {
-      ui.display("    /:''|    ");
-      ui.display("   |: 66|_   ");
-      ui.display("   C     _)  ");
-      ui.display("    \\ ._|      ");
-      ui.display("     ) /       ");
-      ui.display("   /`\\       ");
-      ui.display("   || |Y|       ");
-      ui.display("   || |#|       ");
-      ui.display("   || |#|       ");
-      ui.display("   || |#|       ");
-      ui.display("   :| |=:       ");
-      ui.display("   ||_|,|      ");
-      ui.display("   \\)))||     ");
-      ui.display("|~~~`-`~~~|   ");
-      ui.display("|  Bye    |    ");
-      ui.display("|_________|    ");
-      ui.display("|_________|    ");
-      ui.display("    | ||       ");
-      ui.display("    |_||__        ");
-      ui.display("    (____))    ");
+      ui.display("    /:''|    " + "       /:''|    ");
+      ui.display("   |: 66|_   " + "      |: 66|_   ");
+      ui.display("   C     _)  " + "      C     _)  ");
+      ui.display("    \\ ._|      " + "    \\ ._|      ");
+      ui.display("     ) /       " + "     ) /       ");
+      ui.display("    /`\\       " + "      /`\\       ");
+      ui.display("   || |Y|       " + "   || |Y|       ");
+      ui.display("   || |#|       " + "   || |#|       ");
+      ui.display("   || |#|       " + "   || |#|       ");
+      ui.display("   || |#|       " + "   || |#|       ");
+      ui.display("   :| |=:       " + "   :| |=:       ");
+      ui.display("   ||_|,|      " + "    ||_|,|      ");
+      ui.display("   \\)))||     " + "     \\)))||     ");
+      ui.display("|~~~`-`~~~|   " + "  |~~~`-`~~~|   ");
+      ui.display("|  Jens   |    " + " |  August |    ");
+      ui.display("|_________|    " + " |_________|    ");
+      ui.display("|_________|    " + " |_________|    ");
+      ui.display("    | ||       "+ "     | ||       ");
+      ui.display("    |_||__        " + "  |_||__        ");
+      ui.display("    (____))    " + "     (____))    ");
+        ui.display("Bye^^");
   }
 }
 
